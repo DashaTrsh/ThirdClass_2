73a5b2563aed4989e89681e79c242b0ab3ec0a52
dasha T <olga061200@icloud.com>
2023-12-09 14:52:42 +0300

    new version

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public double calculate (double heightM, double weightKg) {
-        double index = weightKg/ Math.pow(heightM,2);
+    public double calculate(double heightM, double weightKg) {
+        double index = weightKg / Math.pow(heightM, 2);
         return index;
     }
 }
